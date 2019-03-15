9e007f901ac9731c3f7b61928db8562a325d12d9
Alessandro Rezende <alessandropereirarezende@gmail.com>
2019-03-15 19:53:23 -0300

    Simplificando com funçoes lambda.

--- NovosRecursos.java
+++ NovosRecursos.java
@@ -11,27 +11,92 @@ public class NovosRecursos {
     palavras.add("casa do código");
     palavras.add("caelum");
 
-    ComparadorPorTamanho comparador = new ComparadorPorTamanho();
-    palavras.sort(comparador);
+    /*
+    1 opção: Pode ser criar uma classe que implementa a interface Consumer
+    */
+    //ComparadorPorTamanho comparador = new ComparadorPorTamanho();
+    //palavras.sort(comparador);
+
+    /*
+    2 opção: passar diretamente no sort o Comparator
+    */
+    /*palavras.sort(new Comparator<String>() {
+          @Override
+          public int compare(String s1, String s2) {
+              if (s1.length() < s2.length())
+                  return -1;
+              if (s1.length() > s2.length())
+                  return 1;
+              return 0;
+          }
+      });
+      */
+
+    /*
+    3 opção: usar lambda verboso
+    */
+    /*palavras.sort((String s1, String s2) -> {
+          if (s1.length() < s2.length())
+              return -1;
+          if (s1.length() > s2.length())
+              return 1;
+          return 0;
+    });*/
+
+    /*
+    4 opção: usar lambda menos verboso
+    */
+    /*palavras.sort((String s1, String s2) -> {
+          return Integer.compare(s1.length(), s2.length());
+    });*/
+
+    /*
+    4 opção: usar lambda simples
+    */
+    palavras.sort((s1,s2) -> Integer.compare(s1.length(), s2.length()));
+
     System.out.println(palavras);
 
     /*
-    Pode ser criar uma classe que implementa a interface Consumer
-    ou criar uma classe anonima.
+    1 opção: Pode ser criar uma classe que implementa a interface Consumer
     */
-    Consumer<String> consumidor = new ImprimeNaLinha();
+    //Consumer<String> consumidor = new ImprimeNaLinha();
 
-    /*Criação da classe anonima. Usado quando não se vai reaproveitar 
+    /*
+    2 opção:Criação da classe anonima. Usado quando não se vai reaproveitar
       o código definido na classe
     */
-    Consumer<String> consumidorClasseAnonima = new Consumer<String>(){
+    /*Consumer<String> consumidorClasseAnonima = new Consumer<String>(){
         @Override
         public void accept(String s) {
             System.out.println(s);
         }
     };
-
     palavras.forEach(consumidorClasseAnonima);
+    */
+
+    /*
+    3 opção: passar diretamente no forEach o Consumer
+    */
+    /*palavras.forEach(new Consumer<String>(){
+        @Override
+        public void accept(String s) {
+            System.out.println(s);
+        }
+     });*/
+
+     /*
+     4 opção: usar lambda verboso
+     */
+     /*palavras.forEach((String s) -> {
+         System.out.println(s);
+     });
+     */
+
+     /*
+     5 opção: usar lambda simples
+     */
+     palavras.forEach(s -> System.out.println(s));
   }
 
 }
